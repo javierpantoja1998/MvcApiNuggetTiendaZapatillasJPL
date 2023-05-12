ffd9a1b3d7616350e81bdc6da93047d9f7d03828
javierpantoja1998 <cientificodebanco@gmail.com>
2023-05-12 11:07:50 +0200

    Add project files.

---
+++ MvcApiNuggetTiendaZapatillasJPL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcApiNuggetTiendaZapatillasJPL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
